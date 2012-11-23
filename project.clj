242c6d39889a81fc88506cf916c94120b1c1300c
Brian Marick <marick@exampler.com>
2012-11-22 18:01:07 -0600

    Upgrade to Lein 2

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject patterned "0.2.0"
   :description "Adds a `defpatterned` macro"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.4.0"]])
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]
+                   :plugins [[lein-midje "2.0.1"]]}})
